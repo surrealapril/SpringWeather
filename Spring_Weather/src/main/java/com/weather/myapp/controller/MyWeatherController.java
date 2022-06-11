5a4f6cdd11cd02bbf7e596b77e66ee5c80e3e4fd
surrealapril <surrealapril@gmail.com>
2022-06-11 13:50:47 +0900

    apiKey get property

--- Spring_Weather/src/main/java/com/weather/myapp/controller/MyWeatherController.java
+++ Spring_Weather/src/main/java/com/weather/myapp/controller/MyWeatherController.java
@@ -1,5 +1,11 @@
 package com.weather.myapp.controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.sql.Connection;
 import java.text.DateFormat;
 
@@ -11,48 +17,100 @@ import javax.sql.DataSource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.io.support.ResourcePropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class MyWeatherController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(MyWeatherController.class);
-	
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@Inject
 	private DataSource dataSource;
-	
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	public String home(Locale locale, Model model) throws IOException {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
+
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
+
 		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+
+		model.addAttribute("serverTime", formattedDate);
+
+		//DB
 		try {
 			Connection conn = (Connection) dataSource.getConnection();
 			System.out.println("성공 : " + conn);
-			
-			} catch (Exception ex){
-				System.out.println("실패..!");
-				ex.printStackTrace();
-			}
-		
+
+		} catch (Exception ex) {
+			System.out.println("실패..!");
+			ex.printStackTrace();
+		}
+
+		//config.properties api.key 불러오기
+		// 컨테이너 생성
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+		// 환경변수 관리 객체 생성
+		ConfigurableEnvironment env = ctx.getEnvironment();
+		// 프로퍼티 관리 객체 생성
+		MutablePropertySources prop = env.getPropertySources();
+		// 프로퍼티 관리 객체에 프로퍼티 파일 추가
+		try {
+			prop.addLast(new ResourcePropertySource("classpath:properties/config.properties"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// 프로퍼티 정보 얻기
+		String apiKey = env.getProperty("api.key");
+		logger.info("myController apikey : " + apiKey);
+
+		StringBuilder urlBuilder = new StringBuilder(
+				"http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst"); /* URL */
+		urlBuilder.append("?" + URLEncoder.encode("serviceKey", "UTF-8") + "="+URLEncoder.encode(apiKey, "UTF-8")); /* Service Key */
+		urlBuilder.append("&" + URLEncoder.encode("pageNo", "UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /* 페이지번호 */
+		urlBuilder.append("&" + URLEncoder.encode("numOfRows", "UTF-8") + "=" + URLEncoder.encode("1000", "UTF-8")); /* 한 페이지 결과 수 */
+		urlBuilder.append("&" + URLEncoder.encode("dataType", "UTF-8") + "=" + URLEncoder.encode("JSON", "UTF-8")); /* 요청자료형식(XML/JSON) Default: XML */
+		urlBuilder.append("&" + URLEncoder.encode("base_date", "UTF-8") + "=" + URLEncoder.encode("20220611", "UTF-8")); /* ‘21년 6월 28일 발표 */
+		urlBuilder.append("&" + URLEncoder.encode("base_time", "UTF-8") + "=" + URLEncoder.encode("0600", "UTF-8")); /* 06시 발표(정시단위) */
+		urlBuilder.append("&" + URLEncoder.encode("nx", "UTF-8") + "=" + URLEncoder.encode("55", "UTF-8")); /* 예보지점의 X 좌표값 */
+		urlBuilder.append("&" + URLEncoder.encode("ny", "UTF-8") + "=" + URLEncoder.encode("127", "UTF-8")); /* 예보지점의 Y 좌표값 */
+		URL url = new URL(urlBuilder.toString());
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setRequestMethod("GET");
+		conn.setRequestProperty("Content-type", "application/json");
+		System.out.println("Response code: " + conn.getResponseCode());
+		BufferedReader rd;
+		if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+			rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+		} else {
+			rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+		}
+		StringBuilder sb = new StringBuilder();
+		String line;
+		while ((line = rd.readLine()) != null) {
+			sb.append(line);
+		}
+		rd.close();
+		conn.disconnect();
+		System.out.println(sb.toString());
+
 		return "home";
 	}
-	
-	
+
 }
